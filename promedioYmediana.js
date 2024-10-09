bc1542a275d74a0d22917dfa1ff5fdf875223602
AlvaroOrtizGil <alvaro31122004@gmail.com>
2024-10-09 22:42:56 +0200

    Update promedioYmediana.js

--- promedioYmediana.js
+++ promedioYmediana.js
@@ -1,12 +1,24 @@
-<!DOCTYPE html>
-<!-- saved from url=(0078)file:///C:/Users/blanc/OneDrive/Documentos/DesarrolloEntornoCliente/index.html -->
-<html lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-  
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
-<body>
-  <script src="file:///C:/Users/blanc/OneDrive/Documentos/DesarrolloEntornoCliente/promedioYmediana.js"></script>
-  
+//Mediana de un array-->es el valor que se encuentra en el cetro deun array ordenad
+//Es como el valor central
+//promedio = media 
+let poscionMedia=0;
 
-</body></html>
+
+function mediana(array){
+    let arrayOrdenado = array.sort((a, b) => a - b);
+   let longitud=arrayOrdenado.length
+   let posicionMedia = Math.floor(longitud / 2);
+        
+        if (longitud % 2 === 0) { // Si la longitud es par
+            // Promedio de los dos valores centrales
+            console.log( (arrayOrdenado[posicionMedia - 1] + arrayOrdenado[posicionMedia]) / 2);
+        } else { // Si la longitud es impar
+            // Devolver el valor central
+            console.log(arrayOrdenado[posicionMedia]);
+        }
+        
+}
+
+let numero=[1,2,3,4];
+
+ mediana(numero);
